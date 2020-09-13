71282e30ef384960ab5588bea9d3a23a10c92b01
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-09-13 15:31:33 +0530

    test

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(express.json())
 app.use("/api", require("./routes/attendance"))
 app.use("/api", require("./routes/portal"))
 
+app.get("/", (req, res) => {
+  res.send("yo buddy")
+})
 // console.log(process.env.MONGO_PASSWORD)
 // console.log(process.env.MONGO_ADMIN)
 const uri = `mongodb+srv://${process.env.MONGO_ADMIN}:${process.env.MONGO_PASSWORD}@cluster0.ikxbe.mongodb.net/<dbname>?retryWrites=true&w=majority`
